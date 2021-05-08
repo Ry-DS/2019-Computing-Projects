5ac4ac58a624e7457d4474f66ee473cf6683c4ee
Ryan Samarakoon <ryan2002.rs@gmail.com>
2021-05-09 01:30:03 +1000

    Fix module issues

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -11,9 +11,9 @@
     requires com.fasterxml.jackson.databind;
     requires com.fasterxml.jackson.core;
 
-    opens me.ryan_s.xml to javafx.graphics, javafx.fxml;
-    opens me.ryan_s.athletics to javafx.graphics, javafx.fxml;
-    opens me.ryan_s.movie to javafx.graphics, javafx.fxml;
+    opens me.ryan_s.xml to javafx.graphics, javafx.fxml, javafx.base;
+    opens me.ryan_s.athletics to javafx.graphics, javafx.fxml, javafx.base;
+    opens me.ryan_s.movie to javafx.graphics, javafx.fxml, javafx.base;
     opens me.ryan_s.xml.data to com.fasterxml.jackson.databind, javafx.base;
 
 }
